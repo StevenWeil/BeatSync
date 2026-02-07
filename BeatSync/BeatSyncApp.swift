9232a313fb21a1709b8f4a0d0167e52533aa5cf3
Steven Weil <stevenweil4@gmail.com>
2026-02-07 13:02:46 -0500

    Initial Commit

---
+++ BeatSync/BeatSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatSyncApp.swift
+//  BeatSync
+//
+//  Created by student on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BeatSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
